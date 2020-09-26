1058fa6833d0eb4cdf35adc58f9aa9138b4402d3
irham98 <48781295+irham98@users.noreply.github.com>
2020-09-26 15:44:38 +0800

    fourth trial of recording picamera

--- camera.py
+++ camera.py
@@ -4,11 +4,12 @@
 
 import io
 import picamera
-import random
 import logging
 import socketserver
 from threading import Condition
 from http import server
+import json
+import cgi
 
 PAGE = """\
 <html>
@@ -42,6 +43,31 @@ class StreamingOutput(object):
 
 
 class StreamingHandler(server.BaseHTTPRequestHandler):
+    def do_POST(self):
+        if self.path == '/labels':
+            content = PAGE.encode('utf-8')
+            self.send_response(200)
+            self.send_header('Content-Type', 'text/html')
+            self.send_header('Content-Length', str(len(content)))
+            self.end_headers()
+            self.wfile.write(content)
+            ctype, pdict = cgi.parse_header(self.headers.get('content-type'))
+            if ctype != 'application/json':
+                self.send_response(400)
+                self.end_headers()
+                return
+            length = int(self.headers["Content-Length"])
+            raw_message = self.rfile.read(length)
+            message = json.loads(raw_message)
+            print(message['label'])
+            self.send_response(200)
+            self.send_header('Content-type', 'application/json')
+            self.end_headers()
+            self.wfile.write(bytes(json.dumps({'status': 0}), 'utf-8'))
+        else:
+            self.send_error(404)
+            self.end_headers()
+
     def do_GET(self):
         if self.path == '/':
             self.send_response(301)
